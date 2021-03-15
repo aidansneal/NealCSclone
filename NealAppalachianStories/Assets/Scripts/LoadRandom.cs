1c7d83d8cb70a631d3a9c67822d1f539bc279c26
aidansneal <59877743+aidansneal@users.noreply.github.com>
2021-03-15 13:29:49 -0400

    difficulties

---
+++ NealAppalachianStories/Assets/Scripts/LoadRandom.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadRandom : MonoBehaviour
+{
+    public void LoadRandomScene()
+    {
+        int index = Random.Range(1, 3);
+        SceneManager.LoadScene(index);
+        Debug.Log("Scene Loaded");
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
