df3d82167d33bbc94a17acc4f687c97b7b5f17fe
aidansneal <59877743+aidansneal@users.noreply.github.com>
2021-03-17 14:10:55 -0400

    changed the load script

--- NealAppalachianStories/Assets/Scripts/DestinationLoad.cs
+++ NealAppalachianStories/Assets/Scripts/DestinationLoad.cs
@@ -9,16 +9,16 @@ public class DestinationLoad : MonoBehaviour
     //allows player to go back to destination 1 by OnClick
     public void Destination1()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+        SceneManager.LoadScene("Destination1", LoadSceneMode.Single);
     }
 
     public void Destination2()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Destination2", LoadSceneMode.Single);
     }
     public void Destination3()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        SceneManager.LoadScene("Destination3", LoadSceneMode.Single);
     }
 
 
