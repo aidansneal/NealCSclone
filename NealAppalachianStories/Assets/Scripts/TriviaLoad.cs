08299aa5c30d8996d399bd9b35388c947729dab7
aidansneal <59877743+aidansneal@users.noreply.github.com>
2021-03-23 12:48:10 -0400

    i could scream
    and i probably will

---
+++ NealAppalachianStories/Assets/Scripts/TriviaLoad.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class TriviaLoad : MonoBehaviour
+{
+
+    
+    public GameObject correct;
+    public GameObject incorrect;
+
+    //
+    public void HandleInputData(int val)
+    {
+        if(val == 2)
+        {
+            incorrect.SetActive(false);
+            Debug.Log("Correct");
+        } else
+        {
+   
+            correct.SetActive(false);
+            
+            Debug.Log("Incorrect");
+        }
+    }
+   
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
