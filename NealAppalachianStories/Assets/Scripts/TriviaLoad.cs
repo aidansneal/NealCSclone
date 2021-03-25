54bd458d67412ec0c9844d39f8003f4cdf27e206
aidansneal <59877743+aidansneal@users.noreply.github.com>
2021-03-24 20:09:07 -0400

    oh yeah
    all the scenes now have stuff and things

--- NealAppalachianStories/Assets/Scripts/TriviaLoad.cs
+++ NealAppalachianStories/Assets/Scripts/TriviaLoad.cs
@@ -25,8 +25,40 @@ public class TriviaLoad : MonoBehaviour
             Debug.Log("Incorrect");
         }
     }
-   
 
+    //input data for Natural Bridge trivia -- the second value in the choices is the correct one
+    public void HandleInputData2(int val)
+    {
+        if (val == 3)
+        {
+            correct.SetActive(true);
+            incorrect.SetActive(false);
+            Debug.Log("Correct");
+        }
+        else
+        {
+            incorrect.SetActive(true);
+            correct.SetActive(false);
+            Debug.Log("Incorrect");
+        }
+    }
+
+    //input data for Natural Bridge trivia -- the second value in the choices is the correct one
+    public void HandleInputData3(int val)
+    {
+        if (val == 1)
+        {
+            correct.SetActive(true);
+            incorrect.SetActive(false);
+            Debug.Log("Correct");
+        }
+        else
+        {
+            incorrect.SetActive(true);
+            correct.SetActive(false);
+            Debug.Log("Incorrect");
+        }
+    }
 
 
 
